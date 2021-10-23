cb4424d3cfcc3cab8acd91eb2c5c18024035b0b0
Jonas Gacrama <gacrama.jonas@gmail.com>
2021-10-23 20:08:19 +0800

    Rendering of the plane is done
    For some reason I need to have a dimension of  64x64x64

--- Chapter02/learn.py
+++ Chapter02/learn.py
@@ -3,6 +3,8 @@
 import os
 import scipy.io as io
 import numpy as np
+import scipy.ndimage as nd
+import matplotlib.pyplot as plt
 from os import system, name  
 
 def clear():
@@ -17,6 +19,12 @@ def clear():
 
 clear()
 print(f'Current Working Directory: {os.getcwd()}')
-voxels = io.loadmat('./3DShapeNets/volumetric_data/door/30/train/door_000001796_12.mat')['instance']
+voxels = io.loadmat('./3DShapeNets/volumetric_data/airplane/30/train/3e73b236f62d05337678474be485ca_12.mat')['instance']
+voxels = np.pad(voxels, (1,1), 'constant', constant_values = (0,0))
+voxels = nd.zoom(voxels, 2, mode='constant', order=0)
 print(f'Shape of Voxels: {np.shape(voxels)}')
-pass
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d', aspect='auto')
+ax.voxels(voxels, edgecolor="red")
+plt.show()
