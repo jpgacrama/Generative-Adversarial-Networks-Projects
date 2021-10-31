737a8ef2880b1d2ec9f1a1ee87bb91f8ae20c18c
Jonas Gacrama <gacrama.jonas@gmail.com>
2021-10-31 14:27:02 +0800

    Training only for 15 epochs and training both GAN and Encoder

--- Chapter03/run.py
+++ Chapter03/run.py
@@ -404,11 +404,11 @@ if __name__ == '__main__':
     # Define hyperparameters
     data_dir = "data"
     wiki_dir = os.path.join(data_dir, "wiki_crop")
-    epochs = 85
+    epochs = 15
     batch_size = 2
     image_shape = (64, 64, 3)
     z_shape = 100
-    TRAIN_GAN = False
+    TRAIN_GAN = True
     TRAIN_ENCODER = True
     TRAIN_GAN_WITH_FR = False
     fr_image_shape = (192, 192, 3)
