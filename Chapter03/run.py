1142bc77ccc1cea25a9221feb92ba4f534a8f767
Jonas Gacrama <gacrama.jonas@gmail.com>
2021-11-01 21:09:13 +0800

    Changing to just 10 epochs

--- Chapter03/run.py
+++ Chapter03/run.py
@@ -426,7 +426,7 @@ if __name__ == '__main__':
     # Define hyperparameters
     data_dir = "data"
     wiki_dir = os.path.join(data_dir, "wiki_crop")
-    epochs = 85
+    epochs = 10
     batch_size = 2
     image_shape = (64, 64, 3)
     z_shape = 100
